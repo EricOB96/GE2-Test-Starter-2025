545a4a4934f722441f6797c030ff695bb0510353
Eric O'Brien <91565520+EricOB96@users.noreply.github.com>
2025-04-29 10:48:46 +0100

    drone movement

--- scripts/drone.gd
+++ scripts/drone.gd
@@ -1,1 +1,24 @@
-extends Node
+extends RigidBody3D
+
+@export var max_speed = 10.0
+@export var speed = 10.0
+@export var rotSpeed = 5.0
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):		
+
+	var turn = Input.get_axis("left", "right")
+	
+	if abs(turn) > 0:     
+		
+		rotation.y -= rotSpeed * delta * turn
+	
+	var move = Input.get_axis("reverse", "forward")
+	if abs(move) > 0:     
+		translate(Vector3.FORWARD * speed * move * delta)
+		
+	var up = Input.get_axis("down", "up")
+	if up:
+		translate(Vector3.UP * max_speed * up * delta)
+		
